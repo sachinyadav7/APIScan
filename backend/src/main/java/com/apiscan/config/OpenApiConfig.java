d336fda93796418a34c92646e9026084c0553372
Sachin Yadav <sachinyadav7@example.com>
2026-07-30 22:37:43 +0530

    add: backend/src/main/java/com/apiscan/config/OpenApiConfig.java

---
+++ backend/src/main/java/com/apiscan/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.apiscan.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI / Swagger UI configuration.
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("APIScan API")
+                        .description("API Security Testing & Monitoring Platform")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("APIScan Team")
+                                .email("support@apiscan.io")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter your JWT access token")));
+    }
+}
